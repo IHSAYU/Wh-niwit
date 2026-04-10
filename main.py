0eb281c3605d3820ebe2ce86c73d3c7e8b0454b0
IHSAYU <ethanftv6@gmail.com>
2026-04-10 11:40:49 -0400

    Implement 'touch-grass' command in main.py
    Added a new command 'touch-grass' that prompts the user to take a break and enjoy the outdoors.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import random
+import time
 
 print("Welcome to Niwit !")
 print("Enter help for what you can do")
@@ -8,7 +9,18 @@ while True:
 
     if user_input == "help":
         print("roll-dice - rolls a dice")
+        print("touch-grass - makes you touch grass")
     elif user_input == "roll-dice":
         print(random.randint(1, 6))
+    elif user_input == "touch-grass":
+        print("You touched grass!")
+        time.sleep(5)
+        print("You really thought you would get away with that ?")
+        time.sleep(2)
+        print("Type touch-grass in a pathetic open-source program ?")
+        time.sleep(2)
+        print("Now go touch some grass IRL")
+        time.sleep(2)
+        break
     else:
         print("Unknown Command")
