041dc54ca70c2908df7382f0e3f077da7cb4f180
IHSAYU <ethanftv6@gmail.com>
2026-04-10 09:56:29 -0400

    Implement basic CLI with help and dice rolling
    Added a simple command-line interface with help and dice roll functionality.

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+import random
+
 print("Welcome to Niwit !")
+print("Enter help for what you can do")
+
+while True:
+    user_input = input("Niwit>")
+
+    if user_input == "help":
+        print("roll-dice - rolls a dice")
+    elif user_input == "roll-dice":
+        print(random.randint(1, 6))
